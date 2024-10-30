c879a7433a776ead7b18677aa007c0aa798a5683
Vtencouchclimbr <148500829+Vtencouchclimbr@users.noreply.github.com>
2024-10-30 11:34:45 -0600

    pathing logic

--- server/dist/seeds/cleanDb.js
+++ server/dist/seeds/cleanDb.js
@@ -2,11 +2,13 @@ import models from '../models/index.js';
 import db from '../config/connection.js';
 export default async (modelName, collectionName) => {
     try {
-        let modelExists = await models[modelName].db.db.listCollections({
-            name: collectionName
-        }).toArray();
-        if (modelExists.length) {
-            await db.dropCollection(collectionName);
+        if (models[modelName].db.db) {
+            let modelExists = await models[modelName].db.db.listCollections({
+                name: collectionName
+            }).toArray();
+            if (modelExists.length) {
+                await db.dropCollection(collectionName);
+            }
         }
     }
     catch (err) {
--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -3,12 +3,13 @@ import db from '../config/connection.js';
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
+    if(models[modelName].db.db) {
     let modelExists = await models[modelName].db.db.listCollections({
       name: collectionName
     }).toArray()
-
     if (modelExists.length) {
       await db.dropCollection(collectionName);
+    }
     }
   } catch (err) {
     throw err;
